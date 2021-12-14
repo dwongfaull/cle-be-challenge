864d40c13245ceda62c59f32856bb0f06127f7cc
David Wong-Faull <david.wongfaull@gmail.com>
2021-12-13 22:20:56 -0500

    - Pull filtered data out of single-object array - Handle case where a player ID has no results

--- api/pitches.ts
+++ api/pitches.ts
@@ -12,11 +12,18 @@ export default (request: VercelRequest, response: VercelResponse) => {
     const playerPitches = pitches.filter(el => el.pitcherId.toString() === query.playerId);
     // If performance is a concern, we could filter this at startup instead of in response to a query,
     // but that would mean we'd have to restart the service to push new data.
-    // wrap it as per previous API
-    const wrappedPlayer = {
-      pitches: playerPitches
-    };
-    response.status(200).json(wrappedPlayer);
+    if (pitches.length > 0) {
+      // wrap it as per previous API
+      const wrappedPlayer = {
+        pitches: playerPitches
+      };
+      response.status(200).json(wrappedPlayer);
+    } else {
+      const res = {
+        message: `Player with playerId = ${query.playerId} not found!`;
+      }
+      response.status(400).json(res);
+    }
   } else {
     const res = {
       message: 'Must pass in playerId as a query param!';
--- api/players.ts
+++ api/players.ts
@@ -11,11 +11,18 @@ export default (request: VercelRequest, response: VercelResponse) => {
   if (query && query.playerId) {
     // If playerId is passed as query parameter, return player details
     const playerDetail = playerDetails.filter(el => el.playerId.toString() === query.playerId);
-    // wrap it as per previous API
-    const wrappedPlayer = {
-      playerDetail
-    };
-    response.status(200).json(wrappedPlayer);
+    if (playerDetail.length > 0) {    
+      // wrap it as per previous API
+      const wrappedPlayer = {
+        playerDetail: playerDetail[0]
+      };
+      response.status(200).json(wrappedPlayer);
+    } else {
+      const res = {
+        message: `Player with playerId = ${query.playerId} not found!`;
+      }
+      response.status(400).json()
+    }
   } else {
     // Otherwise, return full list of player overviews
     // wrap it as per previous API
