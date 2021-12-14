aa8caced7865e93b7129d71636f1dddfe5e545cc
David Wong-Faull <david.wongfaull@gmail.com>
2021-12-13 21:43:13 -0500

    - First pass at players API

---
+++ api/players.ts
@@ -0,0 +1,27 @@
+import {
+  VercelRequest,
+  VercelResponse
+} from "@vercel/node";
+
+import { playerDetails } from './data/PlayerDetails';
+import { playerOverviews } from './data/PlayerOverviews';
+
+export default (request: VercelRequest, response: VercelResponse) => {
+  const { query } = request;
+  if (query && query.playerId) {
+    // If playerId is passed as query parameter, return player details
+    const playerDetail = playerDetails.filter(el => el.playerId === playerId);
+    // wrap it as per previous API
+    const wrappedPlayer = {
+      playerDetail
+    };
+    response.status(200).json(wrappedPlayer);
+  } else {
+    // Otherwise, return full list of player overviews
+    // wrap it as per previous API
+    const wrappedOverview = {
+      players: playerOverviews
+    };
+    response.status(200).json(wrappedOverview);
+  }
+};
